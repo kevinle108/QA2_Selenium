e13586b4d5a61d59fa179c2d23ab01058454e009
Kevin Le <kevinle108@gmail.com>
2023-03-07 11:03:54 -0500

    add POM elements

--- QA2_Selenium/UnitTests.cs
+++ QA2_Selenium/UnitTests.cs
@@ -3,6 +3,8 @@ using OpenQA.Selenium;
 using QA_2_Browser_Testing.PageObjectModels;
 using Xunit.Abstractions;
 using FluentAssertions;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace QA2_Selenium
 {
@@ -15,8 +17,9 @@ namespace QA2_Selenium
             this.output = output;
         }
 
+        // click the event button to display the event qr generator
         [Fact]
-        public void LoadPage_()
+        public void LoadEventSection()
         {
             using (IWebDriver driver = new ChromeDriver())
             {
@@ -32,6 +35,46 @@ namespace QA2_Selenium
                 
 
                 Thread.Sleep(2000);
+
+
+
+                //string title = driver.Title;
+                //IWebElement houseinfo = driver.FindElement(By.Id("house-info"));
+
+                //title.Should().Be("8110 Kellerman Rd, Louisville, KY 40219 | MLS# 1631002 | Redfin");
+                //houseinfo.Text.Should().Be("");
+                //title.Should().Be("Docs • Svelte");
+
+            }
+        }
+        
+        [Fact]
+        public void ClickDatePicker()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("https://4qrcode.com/");
+
+                var homePage = new HomePage(driver);
+
+                homePage.EventButton.Click();
+
+                homePage.EventTitle.SendKeys("End of Class Party");
+                homePage.EventLocation.SendKeys("Online");
+                homePage.EventStartDatePicker.SendKeys("March 31, 2023 1:00 PM");
+                homePage.EventEndDatePicker.SendKeys("March 31, 2023 5:00 PM");
+                homePage.EventNotes.SendKeys("To celebrate completing the Code Louisville QA track!");
+
+
+                //WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2));
+                //IWebElement datePickerWidget =
+                //    wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("div[@class='bootstrap-datetimepicker-widget dropdown-menu top'")));
+
+
+
+
+                Thread.Sleep(10000);
 
 
 
